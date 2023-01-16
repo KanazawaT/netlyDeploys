51eae1ca4daed05e7a3a321254b04aaaa03f0f5f
KnzwaT <e1q19024@oit.ac.jp>
2023-01-17 00:11:21 +0900

    5人用リアクション

---
+++ live2DCommunication/Build/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -0,0 +1,3 @@
+#include "il2cpp-config.h"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
---
+++ live2DCommunication/Build/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
@@ -0,0 +1,6 @@
+#include "il2cpp-config.h"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
+#include "E:\git\kanazawaSotsuken\Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
